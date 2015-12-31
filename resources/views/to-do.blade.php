6a754f37b37e9f08d672a193bcafdfd840f46bd6
Ubuntu <ubuntu@ip-172-31-28-49.us-west-2.compute.internal>
2015-12-31 17:12:06 +0000

    Create a to-do view for task remembering and distributing purposes

---
+++ resources/views/to-do.blade.php
@@ -0,0 +1,16 @@
+@extends('myLayoutViews.master')
+@section('content')
+<div class="containter">
+	<h1>to do:</h1>
+	<ul>
+		<li>Cambiar el paginador de ponentes de horizontal a uno vertical</li>
+		<li>Diseñar divs del carrusel de ponencias del home</li>
+		<li>Insertar contenido real a los carruseles del home. Las fotos e íconos están en public/img/</li>
+		<li>Diseñar el style-master.css</li>
+		<li>Añadir links a navbar para facilitar navegación</li>
+		<li>Añadir glyphicons a los items de la navbar</li>
+		<li>Hacer que la navbar sea estática y sticky</li>
+		<li>Añadir color a navbar superior</li>
+	</ul>
+</div>
+@stop
