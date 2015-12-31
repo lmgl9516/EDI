614a43843c8c0904163bde0845ec98e4f8d59c48
Ubuntu <ubuntu@ip-172-31-28-49.us-west-2.compute.internal>
2015-12-31 16:30:54 +0000

    Apply style and implement major modifications

--- resources/views/myLayoutViews/master.blade.php
+++ resources/views/myLayoutViews/master.blade.php
@@ -16,8 +16,8 @@
         <!-- Bootstrap -->
         <link rel="stylesheet" href="{{ URL::asset('css/bootstrap/bootstrap.min.css') }}">
 
-        <!-- font awesome -->
-        <link rel="stylesheet" href="{{ URL::asset('css/font-awesome/font-awesome.css') }}">
+        <!-- font awesome CDN -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 
         <!-- animations -->
         <link rel="stylesheet" href="{{ URL::asset('css/animate.css') }}">
@@ -28,11 +28,73 @@
         <!-- EDI master -->
         <link rel="stylesheet" href="{{ URL::asset('css/edi/style-master.css') }}">
 
+        <!-- EDI master -->
+        <link rel="stylesheet" href="{{ URL::asset('css/edi/style-twinbar.css') }}">
+
         @yield('styles')
     </head>
 
     <body>
-        @yield('content')
+            <!-- hidden checkbox controls -->
+            <input type="checkbox" id="left-togglebox">
+            <input type="checkbox" id="right-togglebox">
+
+            <div class="left-overlay"><label for="left-togglebox"></label></div>
+            <div class="right-overlay"><label for="right-togglebox"></label></div>
+
+            <nav id="left-offcanvas-menu">
+                <label for="left-togglebox" id="left-close-x">Close</label>
+                <ul>
+                    <li style="background: #499DA9; padding: 25px 0px 15px 0px;">
+                        <div id="nav-img" style="padding: 10px 60px;">
+                        <img src="https://pbs.twimg.com/profile_images/3777388091/b52a9e0da2486de95accae18dd0fa328_400x400.jpeg" class="img-circle img-responsive" style="box-shadow: 0px 5px 10px rgba(0,0,0,0.5);">
+                        </div>
+                        <p>Emma Stone</p>
+                        <h5>Country Business Manager</h5>
+                    </li>
+                    <div class="text-left" style="padding-left: 15px;">
+                        <li><a href="{{ URL::action('MainController@mpGetHome')}}">                  <span class="fa fa-home"             style="margin-right: 18px;"></span> Home</a></li>
+                        <li><a href="{{ URL::action('MainController@mpGetWorkPlan')}}">              <span class="fa fa-calendar-check-o" style="margin-right: 18px;"></span> Plan de Trabajo</a></li>
+                        <li><a href="{{ URL::action('MainController@mpGetHome')}}">                  <span class="fa fa-bullhorn"         style="margin-right: 18px;"></span> Foro</a></li>
+                        <li><a href="{{ URL::action('PollController@mpGetAllPolls')}}">              <span class="fa fa-send"             style="margin-right: 18px;"></span> Encuestas</a></li>
+                        <li><a href="{{ URL::action('MainController@mpGetHome')}}">                  <span class="fa fa-envelope"         style="margin-right: 18px;"></span> Asistentes</a></li>
+                        <li><a href="{{ URL::action('MainController@mpGetAllSpeakers')}}">           <span class="fa fa-users"            style="margin-right: 18px;"></span> Ponentes</a></li>
+                        <li><a href="{{ URL::action('MainController@mpGetOrganizersAndSponsors')}}"> <span class="fa fa-certificate"      style="margin-right: 18px;"></span> Empresas</a></li>
+                        <li><a href="{{ URL::action('LoginController@mpLogoutUser')}}">              <span class="fa fa-sign-out"         style="margin-right: 18px;"></span> Cerrar sesión</a></li>
+                    </div>
+                </ul>
+            </nav>
+
+            <nav id="right-offcanvas-menu">
+                <label for="right-togglebox" id="right-close-x">Close</label>
+                <ul>
+                    <?php $counter = 0; $background = 'background: #499DA9';
+                     ?>
+                    @if (!is_null($miniPlan))
+                        @foreach ($miniPlan as $talk)
+                            
+                            <?php (($counter % 2) == 0) ? $color = $background = 'background: #499DA9' : $background = '' ?>
+
+                            <li class="text-left" style="padding: 15px 25px; {{$background}}">
+                                <p> <b> <span class="fa fa-clock-o"></span> {{$talk['hora']}} </b> </p>
+                                <p>{{$talk['titulo']}}</p>
+                            </li>
+                            <?php $counter++ ?>
+                        @endforeach
+                    @endif
+                </ul>
+            </nav>
+
+            <div id="content-area">
+                <div class="col-md-6 col-sm-6 col-xs-6 navbar-col">
+                    <label for="left-togglebox" id="left-hamburguer"></label>
+                </div>
+                <div class="col-md-6 col-sm-6 col-xs-6 navbar-col">
+                    <label for="right-togglebox" id="right-hamburguer"></label>
+                </div>
+
+                @yield('content')
+            </div>
     </body>
 
     <!-- AJAX 1.11.2 CDN-->
